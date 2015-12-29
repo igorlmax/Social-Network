cd96ec85417a76fb8d0aba9f35aac4dc40eb4bc7
Igor Litajkovski <igorlmax@yahoo.com>
2015-12-29 23:53:36 +0100

    Extend the documentation of index.php

--- header.php
+++ header.php
@@ -1,10 +1,12 @@
+<?php
+	// Start the Session or continue it
+	session_start();
+?>
+
 <link rel="stylesheet" href="css/style.css" type="text/css">
 <script type="text/javascript" src="js/javascript.js"></script>
 
 <?php
-// Start the Session or continue it
-session_start();
-
 echo "<!DOCTYPE html>\n<html><head>";
 
 // Include the App functions
@@ -34,20 +36,26 @@ else
 /*
  * Load a style sheet, depend on whther the user is loggedin or not
  */
+	
+	// If the User is loggedin
 if($loggedin)
-	echo "<br ><ul class='menu'>" .
-	"<li><a href='members.php?view=$user'>Home</a></li>" .
-	"<li><a href='members.php'>Members</a></li>" .
-	"<li><a href='friends.php'>Friends</a></li>" .
-	"<li><a href='messages.php'>Messages</a></li>" .
-	"<li><a href='profile.php'>Edit Profile</a></li>" .
-	"<li><a href='logout.php'>Log out</a></li></ul><br>";
+	echo "<br >
+			<ul class='menu'>" .
+				"<li><a href='members.php?view=$user'>Home</a></li>" .
+				"<li><a href='members.php'>Members</a></li>" .
+				"<li><a href='friends.php'>Friends</a></li>" .
+				"<li><a href='messages.php'>Messages</a></li>" .
+				"<li><a href='profile.php'>Edit Profile</a></li>" .
+				"<li><a href='logout.php'>Log out</a></li>
+			</ul><br>";
 	
 else
-	echo ("<br><ul class='menu'>" .
-	"<li><a href='index.php'>Home</a></li>" .
-	"<li><a href='signup.php'>Sign up</a></li>" .
-	"<li><a href='login.php'>Log in</a></li></ul><br>" .
-	"<span class='info'>&#8658; You must be logged in to " .
-	"view this page.</span><br><br>");
+	echo ("<br>
+			<ul class='menu'>" .
+				"<li><a href='index.php'>Home</a></li>" .
+				"<li><a href='signup.php'>Sign up</a></li>" .
+				"<li><a href='login.php'>Log in</a></li>
+			</ul><br>" .
+			"<span class='info'>&#8658; You must be logged in to " .
+			"view this page.</span><br><br>");
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-  require_once 'header.php';
-  
-  echo "<br><span class='main'>Welcome to $appname,";
-  
-  if ($loggedin)echo " $user, you are logged in.";
-  else          echo ' please sign up or log in to join in.';
+require_once 'header.php';
+
+echo "<br><span class='main'>Welcome to $appname,";
+
+if ($loggedin)
+	echo " $user, you are logged in.";
+else
+	echo ' please sign up or log in to join in.';
 ?>
-    </span><br><br>
-  </body>
+				</span>
+			<br>
+		<br>
+	</body>
 </html>
